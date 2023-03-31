3259845ffc7212d0f3089d5b7423bd7631aaf208
pnaskardev <priyanshunaskar89@gmail.com>
2023-03-31 19:46:58 +0530

    added controllers and cleaned out the routers

--- ExpressServer/routes/shop.js
+++ ExpressServer/routes/shop.js
@@ -1,17 +1,9 @@
 const express=require('express');
 const router=express.Router();
-const path=require('path');
 
-const rootDir=require('../utils/path_provider');
-const adminData=require('./admin');
-router.get('/',(req,res,next)=>
-{
-    // console.log("In / middleware");
-    // // res.send('<h1>Hello from Express</h1>');
-    // console.log('shopJs',adminData.products);
-    // res.sendFile(path.join(rootDir,'views','shop.html'));
-    const product=adminData.products;
-    res.render('shop',{prods:product,docTitle:'Shop',path:'/'});
-});
+const productsController=require('../controllers/products');
+
+
+router.get('/',productsController.getProducts);
 
 module.exports=router;
