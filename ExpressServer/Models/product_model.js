69445ec1d03fd03f17ed5d971fdab856d8d39be9
pnaskardev <priyanshunaskar89@gmail.com>
2023-03-31 21:03:48 +0530

    moved to mvc architecture

---
+++ ExpressServer/Models/product_model.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const path = require('path');
+
+const path_provider=require('../utils/path_provider');
+
+const p = path.join
+(
+    path_provider,
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
+module.exports = class Product {
+  constructor(t) {
+    this.title = t;
+  }
+
+  save() {
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
