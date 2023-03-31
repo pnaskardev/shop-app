9f911f25f9ae0edd12dad8d79248c044aa59b8b7
pnaskardev <priyanshunaskar89@gmail.com>
2023-03-31 16:08:18 +0530

    added git ignore

--- ExpressServer/routes/admin.js
+++ ExpressServer/routes/admin.js
@@ -5,6 +5,8 @@ const router=express.Router();
 
 const rootDir=require('../utils/path_provider');
 
+const products=[];
+
 router.get('/add-product',(req,res,next)=>
 {
     console.log("In add-product middleware");
@@ -18,10 +20,15 @@ router.get('/add-product',(req,res,next)=>
 });
 
 // .post only handles post requests 
-router.post('/product',(req,res,next)=>
+router.post('/add-product',(req,res,next)=>
 {
     console.log(req.body);
+    products.push
+    ({
+        title:req.body.title
+    });
     res.redirect('/');
 });
 
-module.exports=router;
+exports.routes=router;
+exports.products=products;
