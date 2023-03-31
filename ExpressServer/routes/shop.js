d2284896c36300f86fe426882f3e2da6b83ae75b
pnaskardev <priyanshunaskar89@gmail.com>
2023-03-31 13:53:24 +0530

    started sending files from router

--- ExpressServer/routes/shop.js
+++ ExpressServer/routes/shop.js
@@ -1,10 +1,14 @@
 const express=require('express');
 const router=express.Router();
+const path=require('path');
+
+const rootDir=require('../utils/path_provider');
 
 router.get('/',(req,res,next)=>
 {
     console.log("In / middleware");
-    res.send('<h1>Hello from Express</h1>');
+    // res.send('<h1>Hello from Express</h1>');
+    res.sendFile(path.join(rootDir,'views','shop.html'));
 });
 
 module.exports=router;
