52b8c0b4845b07827958417b8b6cd2f470e2b382
pnaskardev <priyanshunaskar89@gmail.com>
2023-04-10 15:57:57 +0530

    connected the database to the sequelize package

--- ExpressServer/models/product.js
+++ ExpressServer/models/product.js
@@ -1,44 +1,35 @@
+const Sequelize=require('sequelize');
 
+const seq=require('../utils/database');
 
-const db=require('../utils/database');
-const Cart=require('../models/cart');
-
-module.exports = class Product 
-{
-  constructor(id,title, imageUrl, description, price) 
+const Product=seq.define
+(
+  'product',
   {
-    this.id=id,
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
+    id:
+    {
+      type:Sequelize.INTEGER,
+      autoIncrement:true,
+      allowNull:false,
+      primaryKey:true,
+    },
+    title:Sequelize.STRING,
+    price:
+    {
+      type:Sequelize.DOUBLE,
+      allowNull:false
+    },
+    imageUrl:
+    {
+      type:Sequelize.STRING,
+      allowNull:false
+    },
+    description:
+    {
+      type:Sequelize.STRING,
+      allowNull:false
+    }
   }
+);
 
-  save() 
-  {
-    return db.execute
-    (
-      'INSERT INTO products (title,price,imageUrl,description) VALUES (?, ?, ?, ?)',
-      [this.title,this.price,this.imageUrl,this.description]
-    );
-  }
-
-  static deleteById(productId)
-  {
-    
-  }
-
-  static fetchAll() 
-  {
-    return db.execute('SELECT * FROM products');
-  }
-
-  static findById(id)
-  {
-    return db.execute
-    (
-      'SELECT * FROM products WHERE products.id= ?',
-      [id]
-    )
-  }
-};
+module.exports=Product;
