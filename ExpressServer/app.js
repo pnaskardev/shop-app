45cd7389a14acfbd5d60956cfb9e23ee0a224b51
pnaskardev <priyanshunaskar89@gmail.com>
2023-04-10 22:48:20 +0530

    in between the sequelize module

--- ExpressServer/app.js
+++ ExpressServer/app.js
@@ -3,18 +3,11 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const sequelize=require('./utils/database');
 const errorController = require('./controllers/error');
+const sequelize = require('./utils/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
-// db.execute('SELECT * FROM products')
-//     .then((result)=>
-//     {
-//         console.log(result[0], result[1]);
-//     })
-//     .catch(err=>
-//         {
-//             console.log(err);
-//         });
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -26,18 +19,40 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
 
-sequelize.sync().then(result=>
-    {
-        // console.log(result);
-        app.listen(3000);
-    })
-    .catch(err=>
-    {
-        console.log(err);
-    });
+sequelize
+  .sync({ force: true })
+  // .sync()
+  .then(result => {
+    return User.findByPk(1);
+    // console.log(result);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'Max', email: 'test@test.com' });
+    }
+    return user;
+  })
+  .then(user => {
+    // console.log(user);
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
