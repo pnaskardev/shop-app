83919b582f8a6c406c76a02fec796b61878d0b1d
pnaskardev <priyanshunaskar89@gmail.com>
2023-04-12 18:30:48 +0530

    removed on error

--- ExpressServer/app.js
+++ ExpressServer/app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById(1)
+  User.findByPk(1)
     .then(user => {
       req.user = user;
       next();
