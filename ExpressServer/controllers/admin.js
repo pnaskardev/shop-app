ee9eea9061121e657685e8c4cb970c7c8566def6
pnaskardev <priyanshunaskar89@gmail.com>
2023-04-12 16:55:43 +0530

    added some extra account wise features in the code

--- ExpressServer/controllers/admin.js
+++ ExpressServer/controllers/admin.js
@@ -39,8 +39,11 @@ exports.getEditProduct = (req, res, next) =>
     return res.redirect('/');
   }
   const prodId=req.params.productId;
-  Product.findByPk(prodId).then(product=>
+  req.user.getProducts({where:{id:prodId}})
+  // Product.findByPk(prodId)
+  .then(products=>
     {
+      const product=products[0];
       if(!product)
       {
         return res.redirect('/');
@@ -123,7 +126,8 @@ exports.postdeleteProduct=(req,res,next)=>
 
 exports.getProducts = (req, res, next) => 
 {
-  Product.findAll()
+  req.user.getProducts()
+  // Product.findAll()
   .then((products)=>
   {
     res.render('admin/products', 
