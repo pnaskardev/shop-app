26698370dbe29b4eea4394f882622280cf1afbc5
pnaskardev <priyanshunaskar89@gmail.com>
2023-04-19 19:42:44 +0530

    shiftwing to MongoDB

--- ExpressServer/app.js
+++ ExpressServer/app.js
@@ -4,34 +4,35 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const mongoConnect=require('./utils/database');
+const mongoConnect=require('./utils/database').mongoConnect;
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-// const adminRoutes = require('./routes/admin');
-// const shopRoutes = require('./routes/shop');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => 
-// {
-//   // User.findByPk(1)
-//   //   .then(user => {
-//   //     req.user = user;
-//   //     next();
-//   //   })
-//   //   .catch(err => console.log(err));
-// });
+app.use((req, res, next) => 
+{
+  // User.findByPk(1)
+  //   .then(user => {
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch(err => console.log(err));
+  next();
+});
 
-// app.use('/admin', adminRoutes);
-// app.use(shopRoutes);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect.mongoConnect().then(result=>
+mongoConnect(()=>
 {
   app.listen(3000);
-}).catch(err=>console.log(err))
+});
--- ExpressServer/utils/database.js
+++ ExpressServer/utils/database.js
@@ -3,64 +3,38 @@ const mongoDb=require('mongodb');
 
 const MongoClient=mongoDb.MongoClient;
 dotenv.config();
+
+
 let _db;
-// let _db;
-// const mongoConnect=(callback)=>
-// {
-//     MongoClient.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@shopapp.w1bzvyc.mongodb.net/?retryWrites=true&w=majority`)
-//     .then
-//     (
-//         client=>
-//         {
-//             console.log("Connected");
-//             _db=client.db();
-//             callback(client);
-//         }
-//     ).catch
-//     (
-//         err=>
-//         {
-//             console.log(err);
-//             throw(err);
-//         }
-//     );
-// }
-async function mongoConnect() 
+const mongoConnect=callback=>
 {
-    const client = new MongoClient(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@shopapp.w1bzvyc.mongodb.net/?retryWrites=true&w=majority`);
-    try 
+    MongoClient.connect
+    (
+        `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@shop.7snf3mh.mongodb.net/?retryWrites=true&w=majority`
+    )
+    .then(client=>
     {
-        await client.connect();
+        console.log('Connected!');
         _db=client.db();
-        await listDatabases(client);
-    } catch (error) 
+        callback();
+    })
+    .catch(err=>
     {
-        console.log(error);    
-    }
-    finally 
-    {
-        await client.close();
-    }
+        console.log(err);
+        throw err;
+    });
 }
 
-async function listDatabases(client)
-{
-    databasesList = await client.db().admin().listDatabases();
- 
-    console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
 
 const getDb=()=>
 {
     if(_db)
     {
-        return db;
+        console.log('db connected');
+        return _db;
     }
     throw 'No database found';
 }
 
-mongoConnect().catch(console.error);
-// module.exports=mongoConnect;
 exports.mongoConnect=mongoConnect;
 exports.getDb=getDb;
