61f413a24d07e5ff423dbe0ff9d00f6e7190554e
pnaskardev <priyanshunaskar89@gmail.com>
2023-04-24 11:21:14 +0530

    added mongoose

--- ExpressServer/app.js
+++ ExpressServer/app.js
@@ -2,9 +2,12 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dotenv=require('dotenv');
 
 const errorController = require('./controllers/error');
-const mongoConnect=require('./utils/database').mongoConnect;
+const User = require('./models/user');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -13,18 +16,18 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+dotenv.config();
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => 
-{
-  // User.findByPk(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
-  next();
+app.use((req, res, next) => {
+  User.findById('5bab316ce0a7c75f783cb8a8')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
@@ -32,7 +35,27 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(()=>
-{
-  app.listen(3000);
-});
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@shop.7snf3mh.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(result => 
+  {
+    console.log('Mongoose Connected');
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Max',
+          email: 'max@test.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
