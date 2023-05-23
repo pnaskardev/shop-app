c122c1f0876fe34b815c571a402d974922e0e381
pnaskardev <priyanshunaskar89@gmail.com>
2023-05-23 16:45:42 +0530

    added sessions

--- ExpressServer/app.js
+++ ExpressServer/app.js
@@ -3,9 +3,9 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const dotenv=require('dotenv');
-const session=require('express-session');
-
+const dotenv = require('dotenv');
+const session = require('express-session');
+const MongoDbStore = require('connect-mongo');
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
@@ -21,14 +21,31 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+// STORAGE FOR THE SESSIONS 
 app.use
 (
-  session({secret:'my secret', resave:false, saveUninitialized:false})
+  session
+  ({ 
+    secret: 'my secret', 
+    resave: false, 
+    saveUninitialized: false,
+    store:MongoDbStore.create
+    ({
+      mongoUrl:process.env.DB_URI,
+      collection:'sessions'
+    }) 
+  })
 );
-app.use((req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
+
+app.use((req,res,next)=>
+{
+  if(!req.session.user)
+  {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then(user => {
-      req.user = user;
+      req.user= user;
       next();
     })
     .catch(err => console.log(err));
